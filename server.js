3c3adc761575e4d0ebf3b9416642fa010089f94a
Liyan Chen <lic128@eng.ucsd.edu>
2017-11-29 00:42:15 -0800

    update

---
+++ server.js
@@ -0,0 +1,53 @@
+const express= require("express");
+const hbs= require("hbs");
+const fs= require("fs");
+
+const port=process.env.PORT || 3000;
+
+
+var app=express();
+hbs.registerPartials(__dirname+'/views/partials')
+app.use(express.static(__dirname+'/public'));
+app.set("view engine", "hbs");
+
+
+app.use((req, res, next)=>{
+    var now= new Date().toString();
+    var log=`${now}; ${req.method}; ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log+'\n', (err)=>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+})
+
+
+
+
+
+
+app.get('/', (req, res)=>{
+   // res.send("Hello Express!");
+    res.render('home.hbs',{
+        pageTitle:"Home Page",
+        welcomeMessage:"Welcone to my website",
+    });
+});
+app.get("/about", (req, res)=>{
+    res.render("about.hbs", {
+        pageTitle: "About Page",
+    });
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
+});
